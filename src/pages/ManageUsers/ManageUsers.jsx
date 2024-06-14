0e4af4f71b3188554496beaaeef84a85c4e3b253
Luiz Verteiro <luizverteiro1819@gmail.com>
2024-06-14 13:48:02 -0300

    feat: add stylesVariables colors

--- src/pages/ManageUsers/ManageUsers.jsx
+++ src/pages/ManageUsers/ManageUsers.jsx
@@ -1,6 +1,156 @@
+import { useState, useEffect } from "react";
+import { useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-toastify";
+import {
+  Container,
+  TypeSelect,
+  ProfilePic,
+  Title,
+  LoadingStyles,
+} from "./Styles";
+import { RiDeleteBin5Line } from "react-icons/ri";
+import { AiOutlineCloseCircle } from "react-icons/ai";
+import { ModalDeleteItem, Table, SearchBar } from "../../components";
+import { LoadingOutlined } from "@ant-design/icons";
+
+import {
+  useDeleteUsers,
+  useGetUsers,
+  useUpdateUsers,
+} from "../../hooks/querys/user";
+
 export default function ManageUsers() {
+  const [userID, setUserID] = useState("");
+  const [users, setUsers] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [modalDelete, setModalDelete] = useState(false);
+
+  const openModalDelete = () => setModalDelete(true);
+  const closeModalDelete = () => setModalDelete(false);
+
+  const modalCloseButton = <AiOutlineCloseCircle />;
+  const queryClient = useQueryClient();
+
+  const columns = [
+    { field: "imageURL", header: "Foto" },
+    { field: "name", header: "Nome" },
+    { field: "email", header: "Email" },
+    { field: "type", header: "Tipo" },
+    { field: "manage", header: "" },
+  ];
+
+  const selectOptions = [
+    { label: "Administrador", value: true },
+    { label: "Usuário", value: false },
+  ];
+  //formating the users
+  async function formatAllUsers() {
+    const filteredUsers = user.filter((user) =>
+      user?.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    const formattedUsers = await filteredUsers.map((user) => ({
+      imageURL: <ProfilePic src={user?.imageURL} alt={user?.name} />,
+      name: user?.name,
+      email: user?.email,
+      type: (
+        <TypeSelect
+          defaultValue={
+            user?.type
+              ? { label: "Administrador", value: true }
+              : { label: "Usuário", value: false }
+          }
+          onChange={(value) => handleTypeChange(user?._id, value)}
+          options={selectOptions}
+        />
+      ),
+      manage: (
+        <RiDeleteBin5Line
+          style={{ cursor: "pointer" }}
+          onClick={() => {
+            openModalDelete();
+            setUserID(user?._id);
+          }}
+        />
+      ),
+    }));
+
+    setUsers(formattedUsers);
+  }
+  function handleTypeChange(_id, data) {
+    const newUserData = { type: data };
+    updateUser({ _id, newUserData });
+  }
+  function handleUserDelete(_id) {
+    deleteUser(_id);
+    closeModalDelete();
+  }
+
+  //backend calls
+  const { mutate: deleteUser } = useDeleteUsers({
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["users"],
+      });
+      toast.success("Usuario deletado com sucesso!");
+    },
+    onError: (err) => {
+      toast.error("Erro ao excluir usuário.", err);
+    },
+  });
+
+  const { mutate: updateUser } = useUpdateUsers({
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["users"],
+      });
+      toast.success("Usuário atualizado com sucesso!");
+    },
+    onError: (err) => {
+      toast.error("Erro ao atualizar usuário.", err);
+    },
+  });
+
+  const { data: user, isLoading } = useGetUsers({
+    onError: (err) => {
+      toast.error("Erro ao pegar itens", err);
+    },
+  });
+  //
+  useEffect(() => {
+    if (!isLoading && user) {
+      formatAllUsers();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user, isLoading, searchQuery]);
+
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
 
   return (
-    <div> MANAGE USERS </div>
+    <Container>
+        <Title>GERENCIAR USUÁRIOS</Title>
+      <SearchBar
+        placeholder={"Pesquisar usuário"}
+        value={searchQuery}
+        search={handleSearchChange}
+        width="90%"
+      />
+      {isLoading ? (
+        <LoadingStyles>
+          <LoadingOutlined />
+        </LoadingStyles>
+      ) : (
+        <Table columns={columns} data={users} />
+      )}
+      <ModalDeleteItem
+        close={closeModalDelete}
+        handleItemDelete={handleUserDelete}
+        id={userID}
+        modal={modalDelete}
+        modalCloseIcon={modalCloseButton}
+        closeModal={closeModalDelete}
+      />
+    </Container>
   );
 }
---
+++ src/pages/ManageUsers/Styles.js
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+import { breakpoints, colors } from "../../styles/stylesVariables";
+import { Select } from "antd";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Title = styled.div`
+  color: ${colors.font.title};
+  font-size: 36px;
+  margin-left: 3%;
+  text-decoration: underline ${colors.underline};
+  text-underline-offset: 0.5rem;
+  text-decoration-thickness: 3px;
+  @media (max-width: ${breakpoints.tablet}) {
+    font-size: 34px;
+  }
+  @media (max-width: ${breakpoints.mobile}) {
+    font-size: 28px;
+    margin-left: 8%;
+  }
+`;
+
+export const ProfilePic = styled.img`
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+`;
+
+export const TypeSelect = styled(Select)`
+  width: 114px;
+  .ant-select-selector {
+    background-color: ${colors.font.primary};
+    color: ${colors.font.primary} !important;
+    border-radius: 4px;
+    border: 2px solid ${colors.background.primary};
+    height: 4vh;
+    text-align: center;
+    justify-content: center;
+  }
+  .ant-select-selection-item {
+    color: ${colors.font.primary} !important;
+  }
+  .ant-select-arrow {
+    display: none;
+  }
+  .ant-select-dropdown {
+    background-color: ${colors.background.secondary};
+    color: ${colors.font.primary};
+    align-items: center;
+  }
+
+  @media (max-width: ${breakpoints.mobile}) {
+    width: 65%;
+  }
+`;
+
+export const LoadingStyles = styled.div`
+  display: "block";
+  font-size: 24px;
+  margin: 20px auto;
+`;
