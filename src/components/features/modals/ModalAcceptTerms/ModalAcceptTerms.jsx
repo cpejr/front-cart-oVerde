353a1f612fb756a38e89c745702ab6861084ff78
Guilherme Fachinelli <Facnelli@gmail.com>
2024-07-12 12:35:32 -0300

    feat: add modal acceptance terms

---
+++ src/components/features/modals/ModalAcceptTerms/ModalAcceptTerms.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import {
+  CheckboxLabel,
+  Container,
+  Header,
+  Image,
+  Section,
+  StyledCheckBox,
+} from "./Style";
+import Square from "../../../../assets/Home/Square.svg";
+import { Checkbox } from "antd";
+
+export default function ModalAcceptTerms() {
+  return (
+    <Container>
+      <Header>Termo de Aceite e privacidade</Header>
+      <Section>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et
+          libero ligula. Aliquam pharetra suscipit euismod. Aliquam erat
+          volutpat. Duis dapibus nulla eu turpis aliquam ultricies. Duis sed
+          consequat enim. Pellentesque viverra tristique nisi ut suscipit. Proin
+          augue nulla, maximus ullamcorper tincidunt non, consectetur non
+          tellus. Phasellus ullamcorper pellentesque lectus vel egestas.
+          Vestibulum eu ex sit amet leo dignissim laoreet et porta elit.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et
+          libero ligula. Aliquam pharetra suscipit euismod. Aliquam erat
+          volutpat. Duis dapibus nulla eu turpis aliquam ultricies. Duis sed
+          consequat enim. Pellentesque viverra tristique nisi ut suscipit. Proin
+          augue nulla, maximus ullamcorper tincidunt non, consectetur non
+          tellus. Phasellus ullamcorper pellentesque lectus vel egestas.
+          Vestibulum eu ex sit amet leo dignissim laoreet et porta elit.
+        </p>
+      </Section>
+
+      <Image>
+        <img src={Square} alt="Imagem de teste"></img>
+      </Image>
+
+      <Section>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et
+          libero ligula. Aliquam pharetra suscipit euismod. Aliquam erat
+          volutpat. Duis dapibus nulla eu turpis aliquam ultricies. Duis sed
+          consequat enim. Pellentesque viverra tristique nisi ut suscipit. Proin
+          augue nulla, maximus ullamcorper tincidunt non, consectetur non
+          tellus. Phasellus ullamcorper pellentesque lectus vel egestas.
+          Vestibulum eu ex sit amet leo dignissim laoreet et porta elit.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et
+          libero ligula. Aliquam pharetra suscipit euismod. Aliquam erat
+          volutpat. Duis dapibus nulla eu turpis aliquam ultricies. Duis sed
+          consequat enim. Pellentesque viverra tristique nisi ut suscipit. Proin
+          augue nulla, maximus ullamcorper tincidunt non, consectetur non
+          tellus. Phasellus ullamcorper pellentesque lectus vel egestas.
+          Vestibulum eu ex sit amet leo dignissim laoreet et porta elit.
+        </p>
+      </Section>
+      <CheckboxLabel>
+        <StyledCheckBox className="custom-checkbox" />
+        Eu concordo com os termos descritos acima
+      </CheckboxLabel>
+    </Container>
+  );
+}
---
+++ src/components/features/modals/ModalAcceptTerms/Style.js
@@ -0,0 +1,78 @@
+import styled from "styled-components";
+import { breakpoints, colors } from "../../../../styles/stylesVariables";
+import { Checkbox as AntCheckbox } from "antd";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2.5rem;
+  overflow: hidden;
+  align-items: center;
+  text-align: center;
+  padding-bottom: 3rem;
+  text-align: justify;
+  margin-top: 30px;
+  p {
+    font-size: 1.5rem;
+    width: 75%;
+    margin-bottom: 0;
+    @media (max-width: ${breakpoints.tablet}) {
+      font-size: 1rem;
+    }
+    @media (max-width: ${breakpoints.mobile}) {
+      font-size: 0.8rem;
+    }
+  }
+`;
+export const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Header = styled.h1`
+  text-transform: uppercase;
+  text-decoration: underline;
+  text-align: center;
+  @media (max-width: ${breakpoints.mobile}) {
+    font-size: 1.5rem;
+  }
+`;
+
+export const Image = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  > img {
+    width: 20rem;
+    @media (max-width: ${breakpoints.mobile}) {
+      width: 12rem;
+    }
+  }
+`;
+
+export const StyledCheckBox = styled(AntCheckbox)`
+  &.custom-checkbox {
+    .ant-checkbox-checked .ant-checkbox-inner {
+      background-color: ${colors.background.secondary};
+      border-color: ${colors.font.secondary};
+    }
+  }
+`;
+
+export const CheckboxLabel = styled.label`
+  cursor: pointer;
+  font-size: 1.5rem;
+  display: flex;
+
+  align-items: center;
+  gap: 17px;
+  @media (max-width: ${breakpoints.tablet}) {
+    font-size: 1rem;
+  }
+  @media (max-width: ${breakpoints.mobile}) {
+    font-size: 0.8rem;
+    gap: 5px;
+  }
+`;
