cc77673885e791394cdd920e97aa6a350a0410ca
ZeL-ucas <zelucaspomps15@gmail.com>
2024-07-23 20:19:41 -0300

    fix: small error fixes

--- src/pages/Support/Support.jsx
+++ src/pages/Support/Support.jsx
@@ -1,13 +1,9 @@
-import React from "react";
-import Header from "../../components/features/layout/Header/Header";
-import Footer from "../../components/features/layout/Footer/Footer";
 import { Button } from "../../components";
 import { WhatsAppIcon } from "../../assets";
-import { 
-  Container, 
-  HeaderArea, 
+import {
+  Container,
   Image,
-  Title, 
+  Title,
   SubTitle,
   TextArea,
   Text1,
@@ -21,64 +17,50 @@ import {
   WhatsAppImage,
   ButtonText,
   Redirecionar,
-  FooterArea
 } from "./Styles";
 
 export default function Support() {
-
-  return(
+  return (
     <Container>
-      <HeaderArea>
-        <Header />
-      </HeaderArea>
-  
-      <Image alt="ImagemCartaoVerde1"/>  
+      <Image alt="ImagemCartaoVerde1" />
 
-      <Title>
-        APOIE O PROJETO NOSSA HISTÓRIA
-      </Title>
+      <Title>APOIE O PROJETO NOSSA HISTÓRIA</Title>
       <TextArea>
         <Text1>
-          A principal forma de nos apoiar é participando do projeto, ajudando na pesquisa história, enviando vídeos, fotografias, documentos que contam a nossa história. Contribua, compartilhe, divulge!
+          A principal forma de nos apoiar é participando do projeto, ajudando na
+          pesquisa história, enviando vídeos, fotografias, documentos que contam
+          a nossa história. Contribua, compartilhe, divulge!
         </Text1>
         <Text2>
-          Você também pode ajudar a manter vivo esse trabalho de preservação da memória de Bom Despacho fazendo uma doação!
+          Você também pode ajudar a manter vivo esse trabalho de preservação da
+          memória de Bom Despacho fazendo uma doação!
         </Text2>
       </TextArea>
       <DonationArea>
         <QRCodeArea>
-          <SubTitle>
-            PARA DOAR:
-          </SubTitle>
+          <SubTitle>PARA DOAR:</SubTitle>
           <QRCodeAndText>
-            <QRCode alt="QrCode"/>
+            <QRCode alt="QrCode" />
             <QRCodeText>
               Escaneie o código ao lado ou faça um pix para 123.321.123
             </QRCodeText>
           </QRCodeAndText>
         </QRCodeArea>
         <WhatsAppButtonArea>
-          <SubTitle>
-            PARA CONTRIBUIR:
-          </SubTitle>
+          <SubTitle>PARA CONTRIBUIR:</SubTitle>
           <Redirecionar href="https://wa.me/+5531975293805">
-            <Button 
-              width="70%" 
+            <Button
+              width="70%"
               flexDirection="row"
               paddingLeft="0.7rem"
               paddingRight="0.7rem"
             >
-              <WhatsAppImage src={WhatsAppIcon} alt="WhatsAppIcon"/>
-              <ButtonText>
-                Entre em contato pelo WhatsApp
-              </ButtonText>
+              <WhatsAppImage src={WhatsAppIcon} alt="WhatsAppIcon" />
+              <ButtonText>Entre em contato pelo WhatsApp</ButtonText>
             </Button>
           </Redirecionar>
         </WhatsAppButtonArea>
       </DonationArea>
-      <FooterArea>
-        <Footer /> 
-      </FooterArea>
     </Container>
-  )
+  );
 }
