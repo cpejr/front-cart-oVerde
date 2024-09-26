4288850f69391cd0a91ccaa7ea12cd8354167ad0
ZeL-ucas <zelucaspomps15@gmail.com>
2024-09-26 09:30:16 -0300

    fix:fiinally fixing the merge error

--- src/components/features/PixButton/Styles.js
+++ src/components/features/PixButton/Styles.js
@@ -1,25 +1,25 @@
 import styled from "styled-components";
 import { breakpoints } from "../../../styles/stylesVariables";
+import { colors } from "../../../styles/stylesVariables";
 export const ButtonPix = styled.button`
   display: flex;
   justify-content: center;
+  margin-top: 10px;
   align-items: center;
-  height: 40px;
-  width: 240px;
-  background-color: #33603f;
+  height: 50px;
+  width: 93px;
+  background-color: ${colors.font.secondary};
   border: solid 1px #33603f;
   border-radius: 5px;
-  color: #fff;
+  color: ${colors.background.primary};
   cursor: pointer;
   font-weight: bold;
   gap: 7px;
 
   @media (max-width: ${breakpoints.mobile}) {
-    margin: 0.7em;
-    width: 120px;
-    font-size: 0.8em;
+    width: 180px;
   }
   &:hover {
-    background-color: rgba(51, 96, 63, 0.8);
+    opacity: 0.8;
   }
 `;
