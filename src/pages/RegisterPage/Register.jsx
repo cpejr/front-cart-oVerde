bb7f2e248d15b1399f016256a91db87957aacc98
Fernanda Resende <fernandamcresende@gmail.com>
2025-02-13 11:48:29 -0300

    Alteracao Form

--- src/pages/RegisterPage/Register.jsx
+++ src/pages/RegisterPage/Register.jsx
@@ -24,6 +24,7 @@ import {
   import { validationSchemaRegister } from "./Validators";
   import { useForm } from "react-hook-form";
   import { zodResolver } from "@hookform/resolvers/zod";
+  import  FormSubmit from "../../components/features/FormSubmit/FormSubmit";
   
   export default function CadastroPage() {
     const [showPassword, setShowPassword] = useState(false);
@@ -45,76 +46,52 @@ import {
       register,
       handleSubmit,
       formState: { errors },
+      reset, 
     } = useForm({
-      resolver: zodResolver(validationSchemaRegister()), 
+      resolver: zodResolver(validationSchemaRegister()),
     });
   
  
-    const onSubmit = (data) => {
-      console.log("Dados enviados:", data);
+    const handleFormSubmit = (data) => {
+      reset();
     };
-  
+    
     return (
       <Container>
         <Title>{translations.title}</Title>
-  
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <InputsDiv>
-           
-            <div>
-              <InputDefault
-                placeholder={translations.name}
-                {...register("name")}
-              />
-              {errors.name && <ErrorMessage>{errors.name.message}</ErrorMessage>}
-            </div>
-           
-            <div>
-              <InputDefault
-                placeholder="E-mail"
-                {...register("email")}
-              />
-              {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
-            </div>
-           
-            <DivPassword>
-              <div style={{ display: "flex", flexDirection: "column", width: "100%" }}>
-                <InputPassword
-                  placeholder="Senha"
-                  type={showPassword ? "text" : "password"}
-                  {...register("password")}
-                />
-              
-              <Icon onClick={togglePasswordVisibility}>
-                {showPassword ? <OpenedEye /> : <ClosedEye />}
-              </Icon>
-              </div>
-             
-            </DivPassword>
-            {errors.password && <PasswordErrorMessage>{errors.password.message}</PasswordErrorMessage>}
-            
-            <DivPassword>
-              <InputPassword
-                placeholder="Confirmar Senha"
-                type={showConfirmPassword ? "text" : "password"}
-              />
-              <Icon onClick={toggleConfirmPasswordVisibility}>
-                {showConfirmPassword ? <OpenedEye /> : <ClosedEye />}
-              </Icon>
-            </DivPassword>
-          </InputsDiv>
-          
-          <ButtonsContainer>
-            <GoogleButton>
-              <GoogleIcon />
-              {translations.google} <strong style={{ marginLeft: "7px" }}>Google</strong>
-            </GoogleButton>
-  
-            <Button marginTop="30px" marginBottom="30px" type="submit">
-              {translations.text}
-            </Button>
-          </ButtonsContainer>
-        </form>
+    
+        <FormSubmit
+        inputs={[
+          {
+            key: "nome",
+            type: "text",
+            placeholder: "Nome",
+            showEyeIcon: false,
+          },
+          {
+            key: "email",
+            type: "email",
+            placeholder: "E-mail",
+            showEyeIcon: false,
+          },
+          {
+            key: "senha",
+            type: "password",
+            placeholder: "Senha",
+            showEyeIcon: true,
+          },
+          {
+            key: "confirmarSenha",
+            type: "password",
+            placeholder: "Confirmar Senha",
+            showEyeIcon: true,
+          },
+        ]}
+        onSubmit={(data) => console.log("Dados enviados:", data)}
+        schema={validationSchemaRegister()}
+        color="blue"
+        loading={false}
+      />
        
         <DivLogin>
           <Login>{translations.login}</Login>
